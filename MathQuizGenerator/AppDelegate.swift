4d98fbd100b25af25a3b3dc04657c861a36a9009
Solem66 <siegfried.sun@gmail.com>
2017-06-06 21:09:45 -0400

    Initial Commit

---
+++ MathQuizGenerator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MathQuizGenerator
+//
+//  Created by Yu Sun on 6/6/17.
+//  Copyright © 2017 Yu Sun. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MathQuizGenerator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MathQuizGenerator
+//
+//  Created by Yu Sun on 6/6/17.
+//  Copyright © 2017 Yu Sun. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
